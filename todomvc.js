c5e94cb0e4f4eea884007f790f9d0e5fb449aff4
nawazishal <50316441+nawazishal@users.noreply.github.com>
2019-05-27 15:47:31 +0500

    added remove function and arrow function applied

--- todomvc.js
+++ todomvc.js
@@ -1,136 +1,104 @@
-//console.dir(document);
-//console.log(document.domain);
-//console.log(document.URL);
-//document.all[10].textContent="";
-//console.log(document.all[10]);
-//console.log(document.all[11]);
-//console.log(document.all);
-//document.all[6].textContent="Not todos"
-// var newone = document.getElementById('myUL');
-// newone.textContent=5;
-// console.log(document.all);
-// console.log(document.all.myUL);
+let todos = document.getElementById("myUL");
+  let listItems = todos.getElementsByTagName("li");
+  let i;
+  let count = 0;
+// Create a "close" button and append it to each list item
+let myNodelist = document.getElementsByTagName("LI");
+for (i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-
-
-
-
-
-
-
- 
- 
- // To Delete list items
- var close = document.getElementsByClassName("close");
- 
- var i;
- for (i = 0; i < close.length; i++) {
-   close[i].onclick = function() {
-     var div = this.parentElement;
-    
-     div.style.display = "none";
-   }
- }
- //Clear the list i-e Delete all list items
- function function1(){
-     var li=document.querySelectorAll('li');
-     for(var k=0 ; k<li.length ; k++)
-     {
-         li[k].style.display='none';
-     }
- }
-
- 
-
- 
-   
-
-
-
- 
- // Create a new list item
- function newElement() {
-   var li = document.createElement("li");
-   li.className='li';
-   var inputValue = document.getElementById("input").value;
-   var t = document.createTextNode(inputValue);
-   
-         li.appendChild(t);
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     document.getElementById("myUL").appendChild(li);
-   }
-   document.getElementById("input").value = "";
-   
- 
-   var span = document.createElement("span");
-   var txt = document.createTextNode("\u00D7");
-   span.className = "close";
-   span.appendChild(txt);
-   li.appendChild(span);
- 
-   for (i = 0; i < close.length; i++) {
-     close[i].onclick = function() {
-       var div = this.parentElement;
-       div.style.display = "none";
-     }
-   }
- }
-/////complete function
-
-
-
-function fun3() {
-  var x = document.getElementById("myUL");
-  var y = x.getElementsByTagName("li");
-  var i;
-  for (i = 0; i < y.length; i++) {
-    console.log[i];
-
-    if (y[i].classList.toggle('checked') == true) {
-
-      y[i].style.display = 'none';
-    }
-
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = () => {
+    let div = this.parentElement;
+    div.style.display = "none";
   }
 }
 
-/////complete function
-function complete() {  
- 
-  var ul = document.getElementById("myUL");
-  var listItem = ul.getElementsByTagName("li");
-  for(let i=0 ; i< listItem.length ; i++)
-  {
-    let item=listItem[i].childNodes[1].checked;
-    if(item != true)
-    {
-      listItem[i].style.display="none";
+// Create a new list item
+let newElement = () => {
+  let li = document.createElement("li");
+  li.id = 'items';
+  let checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  let date = document.createElement("text");
+  let time = new Date();
+  date.innerHTML = time;
+  date.style.margin = "10px";
+  let inputValue = document.getElementById("input").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  li.appendChild(checkBox);
+  li.appendChild(date);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span)
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+//complete function
+let completedTasks= ()=> {
+  for(i = 0; i < listItems.length; i++) {
+    let itemChecked = listItems[i].childNodes[1].checked;
+    if (itemChecked != true) {
+      listItems[i].style.display = "none";
     }
     else {
-      listItem[i].style.display = "block";
+      listItems[i].style.display = "block";
+      count = count + 1;
     }
-    
   }
+  alert("items completed " + count);
 }
-/////// Active function
-function activefun() {  
-  var ul = document.getElementById("myUL");
-  var listItem = ul.getElementsByTagName("li");
-  for(let i=0 ; i< listItem.length ; i++)
-  {
-    let item=listItem[i].childNodes[1].checked;
-    if(item === true)
-    {
-      listItem[i].style.display="none"; 
+// Active function
+let  uncompletedTasks= ()=> {
+  let count=0;
+  for(i = 0; i < listItems.length; i++) {
+    let itemChecked = listItems[i].childNodes[1].checked;
+    if (itemChecked === true) {
+      listItems[i].style.display = "none";
     }
     else {
-      listItem[i].style.display = "block";
+      listItems[i].style.display = "block";
+      count = count + 1;
     }
-    
+  }
+  alert("reamaining tasks " + count);
+}
+
+//show all function
+let showAllToDos = ()=> {
+  for(i = 0; i < listItems.length; i++) {
+    listItems[i].style.display = "block"
   }
 }
 
-
- 
+//clear completed tasks
+let clearCompletedTasks = ()=> {
+  for(i = 0; i < listItems.length; i++) {
+    let itemChecked = listItems[i].childNodes[1].checked;
+    if (itemChecked === true) {
+      listItems[i].remove();
+      i--; 
+    }
+  }
+}
