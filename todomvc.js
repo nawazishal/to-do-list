7587bc9a3175934c88ec3d60fd2f0ca9f5505023
Nawazish Ali <50316441+nawazishal@users.noreply.github.com>
2019-07-09 16:50:16 +0500

    removed the addition syntex.

--- todomvc.js
+++ todomvc.js
@@ -2,16 +2,6 @@ let todos = document.getElementById("myUL");
 let listItems = todos.getElementsByTagName("li");
 let i;
 let count = 0;
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("LI");
-for (i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
 // Click on a close button to hide the current list item
 let close = document.getElementsByClassName("close");
 for (i = 0; i < close.length; i++) {
