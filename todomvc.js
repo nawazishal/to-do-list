b93d37c806b27b6bc86d2759d0be776adc90495e
Nawazish Ali <50316441+nawazishal@users.noreply.github.com>
2019-07-25 15:53:59 +0500

    addition method updated.

--- todomvc.js
+++ todomvc.js
@@ -2,14 +2,6 @@ let todos = document.getElementById("myUL");
 let listItems = todos.getElementsByTagName("li");
 let i;
 let count = 0;
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = () => {
-    let div = this.parentElement;
-    div.style.display = "none";
-  }
-}
 
 // Create a new list item
 let newElement = () => {
@@ -17,13 +9,7 @@ let newElement = () => {
   li.id = 'items';
   let checkBox = document.createElement("input");
   checkBox.type = "checkbox";
-  let date = document.createElement("text");
-  let time = new Date();
-  date.innerHTML = time;
-  date.style.margin = "10px";
   let inputValue = document.getElementById("input").value;
-  let t = document.createTextNode(inputValue);
-  li.appendChild(t);
   li.appendChild(checkBox);
   li.appendChild(date);
   if (inputValue === '') {
@@ -32,17 +18,7 @@ let newElement = () => {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("input").value = "";
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span)
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  
 }
 
 //complete function
