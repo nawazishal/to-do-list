ce508cc2894010065aab3f8f698a61d10018e57b
Nawazish Ali <nawazishali872@gmail.com>
2019-08-23 16:47:04 +0500

    sample commit

--- todomvc.js
+++ todomvc.js
@@ -6,71 +6,67 @@ let count = 0;
 // Create a new list item
 let newElement = () => {
   let li = document.createElement("li");
-  li.id = 'items';
+  li.id = "items";
   let checkBox = document.createElement("input");
   checkBox.type = "checkbox";
   let inputValue = document.getElementById("input").value;
   let text = document.createTextNode(inputValue);
   li.appendChild(text);
   li.appendChild(checkBox);
-  
-  if (inputValue === '') {
+
+  if (inputValue === "") {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").insertBefore(li,todos.childNodes[0])
+    document.getElementById("myUL").insertBefore(li, todos.childNodes[0]);
   }
   document.getElementById("input").value = "";
-  
-}
+};
 
 //complete function
-let completedTasks= ()=> {
-  for(i = 0; i < listItems.length; i++) {
+let completedTasks = () => {
+  for (i = 0; i < listItems.length; i++) {
     let itemChecked = listItems[i].childNodes[1].checked;
     if (itemChecked != true) {
       listItems[i].style.display = "none";
-    }
-    else {
+    } else {
       listItems[i].style.display = "block";
       count = count + 1;
     }
   }
-}
+};
 // Active function
- uncompletedTasks= ()=> {
-  let count=0;
-  for(i = 0; i < listItems.length; i++) {
+uncompletedTasks = () => {
+  let count = 0;
+  for (i = 0; i < listItems.length; i++) {
     let itemChecked = listItems[i].childNodes[1].checked;
     if (itemChecked === true) {
       listItems[i].style.display = "none";
-    }
-    else {
+    } else {
       listItems[i].style.display = "block";
       count = count + 1;
     }
   }
   alert("reaming Tasks " + count);
-}
+};
 
 //show all function
-let showAllToDos = ()=> {
-  for(i = 0; i < listItems.length; i++) {
-    listItems[i].style.display = "block"
+let showAllToDos = () => {
+  for (i = 0; i < listItems.length; i++) {
+    listItems[i].style.display = "block";
   }
-}
+};
 
 //clear completed tasks
-let clearCompletedTasks = ()=> {
-  for(i = 0; i < listItems.length; i++) {
+let clearCompletedTasks = () => {
+  for (i = 0; i < listItems.length; i++) {
     let itemChecked = listItems[i].childNodes[1].checked;
     if (itemChecked === true) {
       listItems[i].remove();
-      i--; 
+      i--;
     }
   }
-}
-let time=()=>{
-  let time=new Date();
-    console.log(time)
-
-}
+};
+let time = () => {
+  let time = new Date();
+  console.log(time);
+};
